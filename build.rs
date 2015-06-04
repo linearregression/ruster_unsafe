82648a9bdad61a5d0de506c41f04ab6bd3af5637
root <edward>
2015-06-04 12:46:32 -0700

    print out files and folders in build.rs

--- build.rs
+++ build.rs
@@ -14,7 +14,9 @@ fn main() {
 	// emit generated files to OUT_DIR
 	let out_dir = env::var("OUT_DIR").unwrap();
 	let dst = Path::new(&out_dir);
+	println!("OUT_DIR {out_dir}, dst {dst}", out_dir, dst)
 	Command::new(escript).arg("gen_api.erl").arg(dst).status().unwrap();
-	Command::new("mv").arg("-f nif_versions.snippet").arg("src").status().unwrap();
-	Command::new("mv").arg("-f nif_api.snippet").arg("src").status().unwrap();
+	Command::new("ls").arg("-l").arg("*.snippet").status().unwrap();
+	Command::new("mv").arg("-f").arg("nif_versions.snippet").arg("src").status().unwrap();
+	Command::new("mv").arg("-f").arg("nif_api.snippet").arg("src").status().unwrap();
 }
